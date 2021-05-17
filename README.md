6c1d2e783cf9d89df76c314e768d5c06ee13d06d
srishti2007 <64143148+srishti2007@users.noreply.github.com>
2021-05-17 17:02:11 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# profile
+# barter-app-stage-6
+project 82
---
+++ components/AppDrawerNavigator.js
@@ -0,0 +1,29 @@
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { AppTabNavigator } from "./AppTabNavigator";
+import CustomSidebarMenu from "./CustomSidebarMenu";
+import MyBartersScreen from "../screens/MyBartersScreen";
+import SettingScreen from "../screens/SettingScreen";
+import NotificationScreen from "../screens/NotificationScreen";
+
+export const AppDrawerNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: AppTabNavigator,
+    },
+    MyBarters: {
+      screen: MyBartersScreen,
+    },
+    Setting: {
+      screen: SettingScreen,
+    },
+    Notifications: {
+      screen: NotificationScreen,
+    },
+  },
+  {
+    contentComponent: CustomSidebarMenu,
+  },
+  {
+    initialRouteName: "Home",
+  }
+);
